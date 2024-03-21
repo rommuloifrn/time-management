4664358e29536f6215ad10ab5501d3322b0aed32
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-03-21 20:06:06 -0300

    style: removed unused imports

--- src/main/java/com/romm/timemanagement/services/ProjectService.java
+++ src/main/java/com/romm/timemanagement/services/ProjectService.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.romm.timemanagement.entities.Entry;
